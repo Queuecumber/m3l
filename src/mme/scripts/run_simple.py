80260e724cdc3991574917f6d200b4fe89c4625c
Max Ehrlich <max.ehr@gmail.com>
2020-12-23 07:30:35 -0800

    multi-gpu
    Signed-off-by: Max Ehrlich <max.ehr@gmail.com>

--- src/mme/scripts/run_simple.py
+++ src/mme/scripts/run_simple.py
@@ -5,11 +5,17 @@ from mme.data import VariedPatch
 from mme.models import QGACCrab
 from torchjpeg.dct import Stats
 
-stats = Stats(Path("/private/home/mehrlich/compression-robust-pkg/src/compression_robust/stats/cstats.pt"))
 
-lod_dir = Path("/checkpoint/mehrlich/lossless_image_datasets/lossless_image_datasets")
-dm = VariedPatch(root_dir=lod_dir, stats=stats, batch_size=128, num_workers=10)
-qgac = QGACCrab(stats=stats)
+def main():
+    stats = Stats(Path("/private/home/mehrlich/compression-robust-pkg/src/compression_robust/stats/cstats.pt"))
 
-trainer = pl.Trainer(gpus=2, replace_sampler_ddp=False)
-trainer.fit(qgac, dm)
+    lod_dir = Path("/checkpoint/mehrlich/lossless_image_datasets/lossless_image_datasets")
+    dm = VariedPatch(root_dir=lod_dir, stats=stats, batch_size=128, num_workers=10)
+    qgac = QGACCrab(stats=stats)
+
+    trainer = pl.Trainer(gpus=2, replace_sampler_ddp=False, accelerator="ddp", max_epochs=200)
+    trainer.fit(qgac, dm)
+
+
+if __name__ == "__main__":
+    main()
