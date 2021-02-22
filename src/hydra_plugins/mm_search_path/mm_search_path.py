8daae0a6f3f5ab89a3dc23dd0996af7e286606e5
Max Ehrlich <max.ehr@gmail.com>
2021-02-22 14:28:19 -0500

    Add search path plugin
    Signed-off-by: Max Ehrlich <max.ehr@gmail.com>

---
+++ src/hydra_plugins/mm_search_path/mm_search_path.py
@@ -0,0 +1,11 @@
+from hydra.core.config_search_path import ConfigSearchPath
+from hydra.plugins.search_path_plugin import SearchPathPlugin
+from os import environ
+
+
+class MMSearchPathPlugin(SearchPathPlugin):
+    def manipulate_search_path(self, search_path: ConfigSearchPath) -> None:
+        search_path.prepend("mm", f"file:///etc/mm")
+        search_path.prepend("mm", f"file:///usr/share/mm")
+        search_path.prepend("mm", f"file:///usr/local/share/mm")
+        search_path.prepend("mm", f"file://{environ['HOME']}/.local/share/mm")
