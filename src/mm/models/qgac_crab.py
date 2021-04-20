5d555d571df464a0b55a011620266738afce6a99
maxehr <maxehr@umiacs.umd.edu>
2021-04-20 13:44:46 -0400

    Generalize image logging
    Signed-off-by: maxehr <maxehr@umiacs.umd.edu>

--- src/mm/models/qgac_crab.py
+++ src/mm/models/qgac_crab.py
@@ -155,7 +155,7 @@ class QGACCrab(pl.LightningModule):
     def validation_epoch_end(self, validation_step_outputs):
         _, _, _, restored_example = validation_step_outputs[0]
 
-        if _module_available("comet_ml") and isinstance(self.trainer.logger, CometLogger):
+        if hasattr(self.trainer.logger.experiment, "log_image"):
             self.logger.experiment.log_image(to_pil_image(restored_example.squeeze(0)), name="val/restored", step=self.global_step or 0)
 
     def configure_optimizers(self) -> Optimizer:
