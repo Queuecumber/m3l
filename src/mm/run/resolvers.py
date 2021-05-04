6aaaa71f86dff48cef0a8a588973515dcdab0e0e
maxehr <maxehr@umiacs.umd.edu>
2021-05-04 10:39:30 -0400

    fix multiply interpolation
    Signed-off-by: maxehr <maxehr@umiacs.umd.edu>

--- src/mm/run/resolvers.py
+++ src/mm/run/resolvers.py
@@ -6,7 +6,7 @@ import hydra
 from omegaconf import OmegaConf
 
 OmegaConf.register_new_resolver("mm.modpath", lambda: Path(__file__).parent.absolute())
-OmegaConf.register_new_resolver("mm.mul", lambda *args: reduce(args, mul, 1))
+OmegaConf.register_new_resolver("mm.mul", lambda *args: reduce(mul, args, 1))
 OmegaConf.register_new_resolver("mm.sum", lambda *args: sum(args))
 OmegaConf.register_new_resolver("mm.get_method", hydra.utils.get_method)
 OmegaConf.register_new_resolver("mm.get_class", hydra.utils.get_class)
