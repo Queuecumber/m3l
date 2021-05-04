bb27b2e563caa8197c5307a68a48aac7c752c6e2
maxehr <maxehr@umiacs.umd.edu>
2021-05-04 10:39:12 -0400

    Remove search path plugin
    Signed-off-by: maxehr <maxehr@umiacs.umd.edu>

--- src/hydra_plugins/mm_search_path/mm_search_path.py
+++
@@ -1,9 +0,0 @@
-from hydra.core.config_search_path import ConfigSearchPath
-from hydra.plugins.search_path_plugin import SearchPathPlugin
-from os import environ
-
-
-class MMSearchPathPlugin(SearchPathPlugin):
-    def manipulate_search_path(self, search_path: ConfigSearchPath) -> None:
-        search_path.prepend("mm", f"file:///etc/mm")
-        search_path.prepend("mm", f"file://{environ['HOME']}/.config/mm")
